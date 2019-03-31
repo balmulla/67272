78963877c6afae138239ff1454e32b012dcc9047
bmulla <bmulla@qatar.cmu.edu>
2019-03-31 08:08:55 +0000

    login successful

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
     has_many :store_users
     has_many :stores, through: :store_users
+    has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :users
   resources :banks
   resources :demos, only: [:new, :create, :destroy]
+  get 'demos/new', to: 'demos#new', as: :login
+  get 'demos/destroy', to: 'demos#destroy', as: :logout
   root 'demos#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
