8654800e3f2d3a00e137caaf084e8935683da8bf
bmulla <bmulla@qatar.cmu.edu>
2019-03-31 07:41:55 +0000

    routes updated

--- app/models/store.rb
+++ app/models/store.rb
@@ -3,6 +3,6 @@ class Store < ApplicationRecord
     has_many :users, through: :store_users
     
     scope :alphabetical, -> {order("name ASC")}
-    scope :by_breakfast, -> {where("opening_time < ?", Time.strptime("11am", "%I%P").strftime("%H:%M"))}
-    scope :by_lunch, -> {where("opening_time < ?", Time.strptime("4pm", "%I%P").strftime("%H:%M"))}
+#    scope :by_breakfast, -> {where("opening_time < ?", Time.strptime("11am", "%I%P").strftime("%H:%M"))}
+#    scope :by_lunch, -> {where("opening_time < ?", Time.strptime("4pm", "%I%P").strftime("%H:%M"))}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :stores
   resources :users
   resources :banks
+  resources :demos, only: [:new, :create, :destroy]
+  root 'demos#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
