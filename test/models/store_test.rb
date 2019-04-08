1f7ad44a11cd28583bf2b48c01ac1f5b40aede71
Sara <smohanad@andrew.cmu.edu>
2019-04-08 17:57:31 +0000

    completed homepage

--- test/models/store_test.rb
+++ test/models/store_test.rb
@@ -6,27 +6,33 @@ class StoreTest < ActiveSupport::TestCase
   context "Creating a context for stores" do
     # create the objects I want with factories
     setup do 
-      create_stores
+            @CMU = FactoryBot.create(:store, name:"CMU", opening_time:"1am", closing_time:"5pm", latitude:"34.50097", longitude:"-84.97533", exclusive: true)
+            @VCU = FactoryBot.create(:store, name:"VCU", opening_time:"11am", closing_time:"5pm", latitude:"34.50097", longitude:"-84.97533", exclusive: true)
+            @Texas = FactoryBot.create(:store, name:"Texas", opening_time:"5pm", closing_time:"11pm", latitude:"34.50097", longitude:"-84.97533", exclusive: false)
+            @Breakfast_club = FactoryBot.create(:store, name:"CMU", opening_time:"8am", closing_time:"1pm", latitude:"34.50097", longitude:"-84.97533", exclusive: false)
     end
     
     # and provide a teardown method as well
     teardown do
-      remove_stores
+   #   remove_stores
     end
   
 
   should "assert that the proper retaurants open for breakfast" do
     assert @Breakfast_club.breakfast?
     assert @CMU.breakfast?
+    assert_not @VCU.breakfast?
   end
   
   should "assert that the proper retaurants open for lunch" do
     assert @VCU.lunch?
     assert @CMU.lunch?
+    assert_not @Breakfast_club.lunch?
   end
 
   should "assert that the proper retaurants open for dinner" do
     assert @Texas.dinner?
+    assert_not @Breakfast_club.dinner?
   end
   
   should "have a scope 'exclusive' that works" do
--- test/sets/store_contexts.rb
+++ test/sets/store_contexts.rb
@@ -2,10 +2,10 @@ module Contexts
     module StoreContexts
         
         def create_stores
-            @CMU = FactoryBot.create(:store, name:"CMU", kind:"Indian", opening_time:"1am", closing_time:"5pm", latitude:"34.50097", longitude:"-84.97533", exclusive: true)
-            @VCU = FactoryBot.create(:store, name:"VCU", kind:"Arabic", opening_time:"11am", closing_time:"5pm", latitude:"34.50097", longitude:"-84.97533", exclusive: true)
-            @Texas = FactoryBot.create(:store, name:"Texas", kind:"International", opening_time:"5pm", closing_time:"11pm", latitude:"34.50097", longitude:"-84.97533", exclusive: false)
-            @Breakfast_club = FactoryBot.create(:store, name:"CMU", kind:"Chinese", opening_time:"8am", closing_time:"1pm", latitude:"34.50097", longitude:"-84.97533", exclusive: false)
+            @CMU = FactoryBot.create(:store, name:"CMU", opening_time:"1am", closing_time:"5pm", latitude:"34.50097", longitude:"-84.97533", exclusive: true)
+            @VCU = FactoryBot.create(:store, name:"VCU", opening_time:"11am", closing_time:"5pm", latitude:"34.50097", longitude:"-84.97533", exclusive: true)
+            @Texas = FactoryBot.create(:store, name:"Texas", opening_time:"5pm", closing_time:"11pm", latitude:"34.50097", longitude:"-84.97533", exclusive: false)
+            @Breakfast_club = FactoryBot.create(:store, name:"CMU", opening_time:"8am", closing_time:"1pm", latitude:"34.50097", longitude:"-84.97533", exclusive: false)
         end
         
         def remove_stores
